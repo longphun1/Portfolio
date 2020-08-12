83fb5717a65d5448c920f726ff9a3cd04c621d88
Long Phun <longphun90@yahoo.com>
2020-08-12 15:35:13 -0700

    set up deployment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/Index';
-
+import './styles/styles.css'
 
 ReactDOM.render(<Index className="App"/>, document.getElementById('app'));
 
